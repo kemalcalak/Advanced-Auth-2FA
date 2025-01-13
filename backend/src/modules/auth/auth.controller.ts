dc47a24693577c72c76ce725adb50592444ab9de
Ali Kemal Çalak <kemalcalak@gmail.com>
2025-01-13 10:25:43 +0300

    Implement authentication cookies and login functionality

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,4 +1,8 @@
-import { registerSchema } from "../../common/validators/auth.validator";
+import { setAuthenticationCookies } from "../../common/utils/cookie";
+import {
+  loginSchema,
+  registerSchema,
+} from "../../common/validators/auth.validator";
 import { HTTPSTATUS } from "../../config/http.config";
 import { asyncHandler } from "../../middlewares/asyncHandler";
 import { AuthService } from "./auth.service";
@@ -16,10 +20,31 @@ export class AuthController {
       const body = registerSchema.parse({
         ...req.body,
       });
-      const user  = await this.authService.register(body);
+      const user = await this.authService.register(body);
       return res.status(HTTPSTATUS.CREATED).json({
         message: "User registered successfully",
         data: user,
+      });
+    }
+  );
+
+  public login = asyncHandler(
+    async (req: Request, res: Response): Promise<any> => {
+      const userAgent = req.headers["user-agent"];
+      const body = loginSchema.parse({
+        ...req.body,
+        userAgent,
+      });
+      const { email, password } = req.body;
+      const { user, accessToken, refreshToken, mfaRequired } =
+        await this.authService.login(body);
+
+      return setAuthenticationCookies({
+        res,accessToken,refreshToken
+      }).status(HTTPSTATUS.OK).json({
+        message: "User login successfully",
+        user,
+        mfaRequired,
       });
     }
   );
--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,41 +1,100 @@
+import jwt from "jsonwebtoken";
 import { ErrorCode } from "../../common/enums/error-code.enum";
 import { VerificationEnum } from "../../common/enums/verification-code.enum";
-import { RegisterDto } from "../../common/interface/auth.interface";
+import { LoginDto, RegisterDto } from "../../common/interface/auth.interface";
 import { BadRequestException } from "../../common/utils/catch-errors";
 import { fortyFiveMinutesFromNow } from "../../common/utils/date-time";
+import SessionModel from "../../database/models/session.model";
 import UserModel from "../../database/models/user.model";
 import VerificationCodeModel from "../../database/models/verification.model";
+import { config } from "../../config/app.config";
 
 export class AuthService {
-    public async register(registerData: RegisterDto) {
-        const { name, email, password } = registerData;
-    
-        const existingUser = await UserModel.exists({
-          email,
-        });
-    
-        if (existingUser) {
-          throw new BadRequestException(
-            "User already exists with this email",
-            ErrorCode.AUTH_EMAIL_ALREADY_EXISTS
-          );
-        }
-        const newUser = await UserModel.create({
-          name,
-          email,
-          password,
-        });
-    
-        const userId = newUser._id;
-    
-        const verification = await VerificationCodeModel.create({
-          userId,
-          type: VerificationEnum.EMAIL_VERIFICATION,
-          expiresAt: fortyFiveMinutesFromNow(),
-        });
-    
-        return {
-          user: newUser,
-        };
+  public async register(registerData: RegisterDto) {
+    const { name, email, password } = registerData;
+
+    const existingUser = await UserModel.exists({
+      email,
+    });
+
+    if (existingUser) {
+      throw new BadRequestException(
+        "User already exists with this email",
+        ErrorCode.AUTH_EMAIL_ALREADY_EXISTS
+      );
+    }
+    const newUser = await UserModel.create({
+      name,
+      email,
+      password,
+    });
+
+    const userId = newUser._id;
+
+    const verification = await VerificationCodeModel.create({
+      userId,
+      type: VerificationEnum.EMAIL_VERIFICATION,
+      expiresAt: fortyFiveMinutesFromNow(),
+    });
+
+    return {
+      user: newUser,
+    };
+  }
+  public async login(loginData:LoginDto) {
+    const { email, password,userAgent } = loginData;
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      throw new BadRequestException(
+        "Invalid email or password",
+        ErrorCode.AUTH_USER_NOT_FOUND
+      );
+    }
+
+    const isPasswordMatch = await user.comparePassword(password);
+
+    if (!isPasswordMatch) {
+      throw new BadRequestException(
+        "Invalid email or password",
+        ErrorCode.AUTH_USER_NOT_FOUND
+      );
+    }
+
+    const session = await SessionModel.create({
+      userId: user._id,
+      userAgent,
+    });
+
+    const accessToken = jwt.sign(
+      {
+        userId: user._id,
+        sessionId: session._id,
+      },
+      config.JWT.SECRET,
+      {
+        audience:["user"],
+        expiresIn: config.JWT.EXPIRES_IN,
       }
+    )
+
+    const refreshToken = jwt.sign(
+      {
+        userId: user._id,
+        sessionId: session._id,
+      },
+      config.JWT.REFRESH_SECRET,
+      {
+        audience:["user"],
+        expiresIn: config.JWT.REFRESH_EXPIRES_IN,
+      }
+    )
+
+    return {
+      user,
+      accessToken,
+      refreshToken,
+      mfaRequired: false,
+    };
+  }
 }
